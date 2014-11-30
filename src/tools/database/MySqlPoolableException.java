9fd86bd0d9f18e3a2b3a3be1f70e27954509ead1
sromer <info@stephanromer.nl>
2014-11-30 19:11:50 +0100

    Roels database class erbij gezet

---
+++ src/tools/database/MySqlPoolableException.java
@@ -0,0 +1,8 @@
+package com.foobar.storage;
+@SuppressWarnings("serial")
+ 
+public class MySqlPoolableException extends Exception {
+     public MySqlPoolableException(final String msg, Exception e) {
+         super(msg, e);
+     }
+}
